dea5568c3cb2c2eec1412a6d221d1d3e42caa690
Doğukan Tekin <dogukantekin.ce@gmail.com>
2025-02-24 12:27:49 +0300

    header created, css files edited

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+import React from 'react'
+
+export function Header() {
+
+    return (
+        <div className='header'>
+            <div className='profile-icon'>D</div>
+            <div className='buttons'>
+                <a href="#" className='button-child'>Skills</a>
+                <a href="#" className='button-child'>Projects</a>
+                <a href="#" className='button-child'>Hire me</a>
+            </div>
+        </div>
+    )
+};
