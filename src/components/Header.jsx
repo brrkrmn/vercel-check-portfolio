38bc39e8ddf22eb38ccec5ed1a3c6897f2559f1e
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-26 13:39:46 +0300

    debug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
+import { useLanguage } from '../context/LanguageContext';
 import { DarkModeToggle } from './DarkModeToggle';
 import './header.css';
+import LanguageSwitcher from './LanguageSwitcher';
 
 export function Header() {
+    const { translations, language } = useLanguage();
+
+    if (!translations) return null;
+
     return (
         <div className='header'>
             <div className='top-section'>
                 <DarkModeToggle />
+                <LanguageSwitcher />
             </div>
 
             <div className='bottom-section'>
                 <div className='profile-icon'>D</div>
                 <div className='buttons'>
-                    HEHEHEHEHE
+                    <a href="#" className='button-child'>{translations[language].skills}</a>
+                    <a href="#" className='button-child'>{translations[language].projects}</a>
+                    <a href="#" className='button-child button-child-primary'>{translations[language].hire_me}</a>
                 </div>
             </div>
         </div>
