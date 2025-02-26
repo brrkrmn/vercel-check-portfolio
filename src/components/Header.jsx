770df183225fedf2cee6e8dfa538a99616fb1665
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-26 13:34:14 +0300

    debug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,7 @@
-import { useLanguage } from '../context/LanguageContext';
 import { DarkModeToggle } from './DarkModeToggle';
 import './header.css';
 
 export function Header() {
-    const { translations, language } = useLanguage();
-
-    if (!translations[language]) return null;
-
     return (
         <div className='header'>
             <div className='top-section'>
@@ -16,9 +11,7 @@ export function Header() {
             <div className='bottom-section'>
                 <div className='profile-icon'>D</div>
                 <div className='buttons'>
-                    <a href="#" className='button-child'>{translations[language].skills}</a>
-                    <a href="#" className='button-child'>{translations[language].projects}</a>
-                    <a href="#" className='button-child button-child-primary'>{translations[language].hire_me}</a>
+                    HEHEHEHEHE
                 </div>
             </div>
         </div>
