230fc6575656f808d6b1db2c214618b9093ad462
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-26 13:29:23 +0300

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import './App.css'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { Introduce } from './components/Introduce'
-import { Profile } from './components/Profile'
-import { Projects } from './components/Projects'
-import { Skills } from './components/Skills'
-import { LanguageProvider } from './context/LanguageContext'
-
 function App() {
 
   return (
     <>
-      <LanguageProvider>
-        <Header />
-        <Introduce />
-        <Skills />
-        <Profile />
-        <Projects />
-        <Footer />
-      </LanguageProvider>
+      HERE
     </>
   )
 }
