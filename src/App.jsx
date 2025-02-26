d5d73472f6aa47c16c537d1ecbf276f41d735354
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-02-26 13:45:44 +0300

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
+import { Introduce } from './components/Introduce'
+import { Profile } from './components/Profile'
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Footer />
+        <Header />
+        <Introduce />
+        <Skills />
+        <Profile />
+        <Projects />
+        <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useLanguage } from '../context/LanguageContext';
 import { DarkModeToggle } from './DarkModeToggle';
 import './header.css';
 import LanguageSwitcher from './LanguageSwitcher';
 
 export function Header() {
+    const { translations, language } = useLanguage();
+
+    if (!translations[language]) return null;
 
     return (
         <div className='header'>
@@ -14,6 +18,9 @@ export function Header() {
             <div className='bottom-section'>
                 <div className='profile-icon'>D</div>
                 <div className='buttons'>
+                    <a href="#" className='button-child'>{translations[language].skills}</a>
+                    <a href="#" className='button-child'>{translations[language].projects}</a>
+                    <a href="#" className='button-child button-child-primary'>{translations[language].hire_me}</a>
                 </div>
             </div>
         </div>
